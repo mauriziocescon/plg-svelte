fbf1fb08016e465effeec16e996583ef264e14d5
Maurizio Cescon <maurizio.cescon@gmail.com>
2026-09-01 12:48:57 +0200

    chore: update Img component props to support attachments and fix attribute spreading order

--- src/routes/foo/wrapper/Img.svelte
+++ src/routes/foo/wrapper/Img.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-	import type { HTMLImgAttributes } from 'svelte/elements';
+	import type { Attachment } from 'svelte/attachments';
 
-	type Props = HTMLImgAttributes & {
+	type Props = {
 		src: string;
 		alt: string;
 		loading?: 'lazy' | 'eager';
+		// Accept attachments that target the element this component renders.
+		[key: symbol]: Attachment<HTMLImageElement> | false | undefined | null;
 	};
 
 	let { src, alt, loading = 'lazy', ...rest }: Props = $props();
 </script>
 
-<img {src} {alt} {loading} {...rest} />
+<img {...rest} {alt} {loading} {src} />
